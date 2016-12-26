50e8969f6e3f5740c2171137df1afd21d53be53e
SK-CSE <saurabh.cse04@gmail.com>
2016-12-26 16:34:28 +0530

    basic form completed

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+	nodemon = require('gulp-nodemon');
+
+gulp.task('default', function(){
+	nodemon({
+		script: 'app.js',
+		ext: 'js',
+		env: {
+			PORT:3000
+		},
+		ignore: ['./node_modules/**']
+	})
+	.on('restart', function(){
+		console.log("restarting");
+	});
+});
