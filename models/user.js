d0d4a2d1a4f473fa6e76ee767d384f73249cffe1
SK-CSE <saurabh.cse04@gmail.com>
2016-12-27 22:03:31 +0530

    updated

--- models/user.js
+++ models/user.js
@@ -1,1 +1,32 @@
-user.js
+var mongoose = require('mongoose');
+var	Schema = mongoose.Schema;
+var bcrypt = require('bcryptjs');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/userslogin');
+var db = mongoose.connection;
+
+var UserSchema = new Schema({
+
+	username: {
+		type: String,
+		index:true
+	},
+
+	password: {
+		type: String
+	},
+
+	email: {
+		type: String
+	},
+
+	name: {
+		type: String
+	}
+});
+
+var User = module.exports = mongoose.model('user',UserSchema);
+
+module.exports.createUser = function(newUser, callback){
+	// 9:00
+}	
--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,31 @@ router.get('/login', function(req, res) {
   res.render('login');
 });
 
+// Register User
+router.post('/register', function(req, res) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	// Validation
+	req.assert('name','Name is required').notEmpty();
+	req.assert('email','email is required').notEmpty();
+	req.assert('email','email is not valid').isEmail();
+	req.assert('username','username is required').notEmpty();
+	req.assert('password','password is required').notEmpty();
+	req.assert('password2','passwords do not match').equals(req.body.password);
+
+
+	var errors = req.validationErrors();
+	if (errors) {
+	  res.render('register',{
+	  	errors : errors
+	  });
+	}else{
+	  console.log("passed");
+	}
+});
+
 module.exports = router;
