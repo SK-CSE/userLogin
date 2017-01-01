14a7e33dd56ffee49ca1c760ba09e4d740ec69ac
SK-CSE <saurabh.cse04@gmail.com>
2017-01-01 19:19:26 +0530

    updated for ensure auth

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ app.use(function(req, res, next){
 	res.locals.success_msg = req.flash('success_msg');
 	res.locals.error_msg = req.flash('error_msg');
 	res.locals.error = req.flash('error'); //passport error
+	res.locals.user = req.user || null;
 	next();
 });
 
